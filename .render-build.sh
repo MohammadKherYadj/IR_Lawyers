4849245fba0215a9d2ee8363a48fabbdcf6caa1b
MohammadKherYadj <mohammadkher509@gmail.com>
2024-11-29 19:13:44 -0900

    update JDK

--- .render-build.sh
+++ .render-build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 apt-get update
-apt-get install -y openjdk-11-jdk
-export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
+apt-get install -y openjdk-22-jdk
+export JAVA_HOME=/usr/lib/jvm/java-22-openjdk-amd64
 export PATH=$JAVA_HOME/bin:$PATH
 pip install -r requirements.txt
