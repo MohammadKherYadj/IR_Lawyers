544d59804a0f017ab6283cd243957c303b540a97
MohammadKherYadj <mohammadkher509@gmail.com>
2024-11-29 19:47:13 -0900

    remove files

--- .render-build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-apt-get update
-apt-get install -y openjdk-22-jdk
-export JAVA_HOME=/usr/lib/jvm/java-22-openjdk-amd64
-export PATH=$JAVA_HOME/bin:$PATH
-pip install -r requirements.txt
